84b3afb56813b637ad3b0c6f15d4655e7d64f6dd
Karson Nichols <nicholsk18@gmail.com>
2020-05-05 16:56:09 -0500

    working on connecting to db

---
+++ server/routers/pageData.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const HomeData = require("../models/homeData");
+const AboutData = require("../models/homeData");
+const ContactData = require("../models/homeData");
+const TeamData = require("../models/homeData");
+const TournamentsData = require("../models/homeData");
+
+const router = new express.Router();
+
+module.exports = router;
