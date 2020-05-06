1317ba0c91d084dcac1c92706a66acc423650473
Karson Nichols <nicholsk18@gmail.com>
2020-05-06 07:38:59 -0500

    import data

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const form = require("./routers/form");
+const pageDataRouter = require("./routers/pageData");
 
 const app = express();
 
 app.use(express.json());
+
 app.use(userRouter);
 app.use(form);
+app.use(pageDataRouter);
 
 module.exports = app;
